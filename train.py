2a004a070af179dd317c450feb3c73ab938341c5
webpagearshi <arshisaleh@gmail.com>
2020-11-03 20:16:53 +0530

    updating train.py file

--- train.py
+++ train.py
@@ -10,19 +10,7 @@ import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = ### YOUR CODE HERE ###
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
-
-run = Run.get_context()
 
 def clean_data(data):
     # Dict for cleaning data
@@ -50,6 +38,7 @@ def clean_data(data):
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
     
+    return x_df, y_df
 
 def main():
     # Add arguments to script
@@ -60,8 +49,23 @@ def main():
 
     args = parser.parse_args()
 
+    run = Run.get_context()
+
     run.log("Regularization Strength:", np.float(args.C))
     run.log("Max iterations:", np.int(args.max_iter))
+
+    # TODO: Create TabularDataset using TabularDatasetFactory
+	# Data is located at:
+	# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+
+	ds = ### YOUR CODE HERE ###
+
+	x, y = clean_data(ds)
+
+	# TODO: Split data into train and test sets.
+
+	### YOUR CODE HERE ###a
+
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
