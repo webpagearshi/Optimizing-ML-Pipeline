6e499b29f6d782e8401e5ffecc6f8ceade85dd96
webpagearshi <arshisaleh@gmail.com>
2020-11-04 18:10:26 +0530

    creating dataset and splitting it into train and test sets

--- train.py
+++ train.py
@@ -37,7 +37,7 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
-    
+
     return x_df, y_df
 
 def main():
@@ -58,14 +58,16 @@ def main():
 	# Data is located at:
 	# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-	ds = ### YOUR CODE HERE ###
-
+	#ds = ### YOUR CODE HERE ###
+	ds = Dataset.Tabular.from_delimited_files(path = 
+		"https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv")
+	
 	x, y = clean_data(ds)
 
 	# TODO: Split data into train and test sets.
 
 	### YOUR CODE HERE ###a
-
+	x_train, x_test, y_train, y_test = train_test_split(x_df, y_df, test_size =0.2, random_state=223)
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
