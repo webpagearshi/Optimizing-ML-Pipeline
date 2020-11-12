e3b9e9ef03800c29b45c182bd65e7a3bd67064a1
webpagearshi <arshisaleh@gmail.com>
2020-11-12 19:16:53 +0530

    updating files

--- train.py
+++ train.py
@@ -59,7 +59,7 @@ def main():
 	# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
 	#ds = ### YOUR CODE HERE ###
-	ds = Dataset.TabularDatasetFactory.from_delimited_files(path = 
+	ds = TabularDatasetFactory.from_delimited_files(path = 
 		"https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv")
 	
 	x, y = clean_data(ds)
@@ -67,12 +67,17 @@ def main():
 	# TODO: Split data into train and test sets.
 
 	### YOUR CODE HERE ###a
-	x_train, x_test, y_train, y_test = train_test_split(x_df, y_df, test_size =0.2, random_state=223)
+	x_train, x_test, y_train, y_test = train_test_split(x, y, test_size =0.2, random_state=223)
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
     accuracy = model.score(x_test, y_test)
-    run.log("Accuracy", np.float(accuracy))
 
+    run.log("Accuracy", np.float(accuracy)) 
+    os.makedirs('outputs', exist_ok = True)
+
+    
+    joblib.dump(value = model, filename= 'outputs/model.pkl')
+	
 if __name__ == '__main__':
     main()
